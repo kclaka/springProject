29855980d6dbb3a2fa2901d1a1913804518ce362
kclaka <kennyigbechi@gmail.com>
2020-02-28 22:37:00 -0800

    Ini

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,8 +3,11 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
@@ -14,7 +17,7 @@ public class PersonController {
         this.personService = personService;
     }
 
-    public void addPerson(Person person){
+    public void addPerson(@RequestBody Person person){
         personService.addPerson(person);
     }
 }
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -5,6 +5,7 @@ import com.example.demo.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class PersonService {
@@ -15,6 +16,7 @@ public class PersonService {
         this.personDao = personDao;
     }
 
+    @PostMapping
     public int addPerson(Person person){
         return personDao.insertPerson(person);
     }
